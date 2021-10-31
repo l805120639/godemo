fa184c41cf969f2d39588c12c068a4be6f70b6a4
l805120639 <85896850+l805120639@users.noreply.github.com>
2021-10-31 22:40:15 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lesson15
+
+go 1.16
+
+require github.com/astaxie/beego v1.12.3
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+
+func main() {
+// now you start the beego as http server.
+// it will listen to port 8080
+beego.I
+
+// it will listen to 8080
+// beego.Run("localhost")
+
+// it will listen to 8089
+// beego.Run(":8089")
+
+// it will listen to 8089
+// beego.Run("127.0.0.1:8089")
+}
